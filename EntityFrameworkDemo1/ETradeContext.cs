e8decc8769f32af6dcbb0c01a6496d09c724d772
EnesAGCA <80221817+EnesAGCA@users.noreply.github.com>
2022-12-27 10:57:54 +0300

    Ürünler ekranı
    entity framework ile veritabanından ürün alma ,silme,ekleme ve güncelleme

---
+++ EntityFrameworkDemo1/ETradeContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkDemo;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo1
+{
+    public class ETradeContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ EntityFrameworkDemo1/ProductDal.cs
@@ -0,0 +1,51 @@
+﻿using EntityFrameworkDemo1;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ProductDal
+    {
+
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context=new ETradeContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+        public void Upate(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State= EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+        public void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State=EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
